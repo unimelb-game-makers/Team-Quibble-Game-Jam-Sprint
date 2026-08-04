57f72ca1b0043e36ce776b12e18eedc36983e575
Hamish Luke <hamishrluke@gmail.com>
2026-08-04 19:29:49 +1000

    added: json for potions

---
+++ cozy-potion-game/resources/potion_ingredient.gd
@@ -0,0 +1,14 @@
+class_name PotionIngredient
+extends Resource
+
+@export var name: String
+@export var sprite: Texture
+
+
+@export var healing: float = 0.0
+@export var energy: float = 0.0
+@export var cure_disease: float = 0.0
+@export var poison: float = 0.0
+
+@export var instant: float = 0.0
+@export var overtime: float = 0.0
--- cozy-potion-game/scripts/potion_crafting/potion_brewing.gd
+++ cozy-potion-game/scripts/potion_crafting/potion_brewing.gd
@@ -1,11 +1,26 @@
+class_name PotionBrewing
 extends Node
 
+var potion_index: Dictionary[String, PotionIngredient]
+var json_path: String = "res://scripts/potion_crafting/potion_list.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	read_json_data()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+func read_json_data() -> void:
+	var json_string: String = FileAccess.open(json_path, FileAccess.READ).get_as_text()
+	var json_data: JSON = JSON.new()
+
+	assert(json_data.parse(json_string) == OK, 
+			"Variable json_data was null. %s" % [json_data.get_error_message()])
+
+	var potion_data = json_data.data
+
+	print(potion_data)
+
