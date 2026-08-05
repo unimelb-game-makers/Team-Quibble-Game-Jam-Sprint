26c6568dff817c00657824e335562e2185884645
Hamish Luke <hamishrluke@gmail.com>
2026-08-05 10:59:21 +1000

    added: creating ingredients from JSON

--- cozy-potion-game/scripts/potion_crafting/potion_brewing.gd
+++ cozy-potion-game/scripts/potion_crafting/potion_brewing.gd
@@ -1,8 +1,9 @@
 class_name PotionBrewing
 extends Node
 
-var potion_index: Dictionary[String, PotionIngredient]
+var potion_ingredient_index: Dictionary[String, PotionIngredient]
 var json_path: String = "res://scripts/potion_crafting/potion_list.json"
+var placeholder: Texture = preload("res://icon.svg")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,7 +21,21 @@ func read_json_data() -> void:
 	assert(json_data.parse(json_string) == OK, 
 			"Variable json_data was null. %s" % [json_data.get_error_message()])
 
-	var potion_data = json_data.data
+	var potion_data = json_data.data["potions"]
 
-	print(potion_data)
+	# this should be changed for a more efficent option
+	for entry in potion_data:
+		var temp_potion: PotionIngredient = PotionIngredient.new()
+		temp_potion.name = entry["name"]
+		temp_potion.sprite = placeholder
 
+		temp_potion.healing = entry["healing"]
+		temp_potion.energy = entry["energy"]
+		temp_potion.cure_disease = entry["cure_disease"]
+		temp_potion.poison = entry["poison"]
+
+		temp_potion.instant = entry["instant"]
+		temp_potion.overtime = entry["overtime"]
+
+		potion_ingredient_index[temp_potion.name] = temp_potion.duplicate()
+		
--- cozy-potion-game/scripts/ui/cauldron_crafting_ui.gd
+++ cozy-potion-game/scripts/ui/cauldron_crafting_ui.gd
@@ -1,22 +1,56 @@
 extends CanvasLayer
 
+@export var potion_brewer: PotionBrewing
 @export var label_money_counter: Label
 @export var container_recipe_list: VBoxContainer
+@export var reset_button: Button
+
+var current_potion: Array[PotionIngredient]
+var ingredient_button: PackedScene = preload("uid://b6r6ktehhfb10")
 
 var player_money: int = 0 :
-    get:
-        return player_money
-    set(_value):
-        player_money = _value
-        label_money_counter.text = "$%010d" % player_money
+	get:
+		return player_money
+	set(_value):
+		player_money = _value
+		label_money_counter.text = "$%s" % player_money
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	player_money = 0
+	await potion_brewer.ready
+
+	reset_button.pressed.connect(reset_potion)
+
+	for entry in potion_brewer.potion_ingredient_index.values():
+		var ingredient: Button = ingredient_button.instantiate()
+		ingredient.text = entry.name
+		ingredient.pressed.connect(add_ingredient.bind(ingredient, entry))
+		container_recipe_list.add_child(ingredient)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-    pass
+	pass
 
+func add_ingredient(_pressed_button: Button, _ingredient: PotionIngredient) -> void:
+	if current_potion.has(_ingredient):
+		return
+
+	if current_potion.size() >= 2:
+		for button: Button in container_recipe_list.get_children():
+			button.disabled = true
+
+	print("Adding %s to potion" % _ingredient.name)
+	current_potion.append(_ingredient)
+	_pressed_button.disabled = true
+
+
+	
+
+func reset_potion() -> void:
+	current_potion.clear()
+
+	for button: Button in container_recipe_list.get_children():
+		button.disabled = false
