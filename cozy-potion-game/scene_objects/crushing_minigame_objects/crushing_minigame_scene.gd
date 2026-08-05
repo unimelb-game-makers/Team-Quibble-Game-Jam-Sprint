679ba092a045d37a607ee3b1b4f31ddec44307ca
Ramsey Morris <118049528+razorarcanum@users.noreply.github.com>
2026-08-05 22:11:50 +1000

    style
    Style updates, no actual changes

--- cozy-potion-game/scene_objects/crushing_minigame_objects/crushing_minigame_scene.gd
+++ cozy-potion-game/scene_objects/crushing_minigame_objects/crushing_minigame_scene.gd
@@ -2,24 +2,28 @@
 extends Control
 
 @export var win_label_placeholder: RichTextLabel
+@export var win_check_interval: float = 0.1
 var win_check_timer: float
-const win_check_interval: float = 0.1
+
 
 #checks all the objects with tag ingredient ball to see if any have not reached their split limit
 #runs every 0.1 seconds or so
-func check_if_won():
+func check_if_won() -> void:
 	for object in get_tree().get_nodes_in_group("IngredientBall"):
-		print_debug(object)
 		if object is IngredientBall:
 			if object.split_count < object.split_limit:
 				return
+	
 	win_minigame()
+
 
 func _process(delta: float) -> void:
 	win_check_timer += delta
+	
 	if win_check_timer > win_check_interval:
 		check_if_won()
 		win_check_timer = 0
-		
-func win_minigame():
+
+
+func win_minigame() -> void:
 	win_label_placeholder.show()
--- cozy-potion-game/scene_objects/crushing_minigame_objects/ingredient_ball.gd
+++ cozy-potion-game/scene_objects/crushing_minigame_objects/ingredient_ball.gd
@@ -1,8 +1,7 @@
+class_name IngredientBall extends RigidBody2D
 ## A gravity-affected rigidbody.
 ## if it collides with an object with the mortar class at a certain
 ## velocity, it splits in two, up to a limit
-extends RigidBody2D
-class_name IngredientBall
 
 #max splits
 @export var split_limit: int
@@ -15,18 +14,16 @@ class_name IngredientBall
 #scale of new balls
 @export var split_scale: float
 #godot doesn't allow scaling rigidbodies, so we have to do a bit of a workaround
-var internal_scale = 1
 @export var polygon: Polygon2D
 @export var collision_polygon: CollisionPolygon2D
 @export var collision_area: Area2D
 @export var texture: Control
-
+var internal_scale = 1
 
 
 #if this ball can still split, for each marker in the split markers array,
 #generate a smaller duplicate of this ball and increment its split count, then put it on that marker
-func split():
-	# there are much smarter and cooler ways to do this
+func split() -> void:
 	if split_count < split_limit:
 		for marker in split_markers:
 			var new_ball: IngredientBall = duplicate()
@@ -34,32 +31,28 @@ func split():
 			new_ball.global_position = marker.global_position
 			new_ball.set_internal_scale(internal_scale * split_scale)
 			get_parent().call_deferred("add_child", new_ball)
+		
 		queue_free()
+
 
 #update the internal scale of the rigidbody and scale all of its children
 #to match. the markers are also moved inward
 #oh my god this is so bad
-func set_internal_scale(new_scale: float):
+func set_internal_scale(new_scale: float) -> void:
 	internal_scale = new_scale
 	polygon.scale = Vector2.ONE * internal_scale
 	collision_polygon.scale = Vector2.ONE * internal_scale
 	collision_area.scale = Vector2.ONE * internal_scale
 	texture.scale = Vector2.ONE * internal_scale
+	
 	for marker in split_markers:
 		marker.position *= new_scale
-	
-#func _on_body_entered(body: Node) -> void:
-	#if body is Pestle:
-		#print_debug(body.velocity.length())
-		#if body.velocity.length() > min_split_vel:
-			#
-			#split()
+
 
 # colliding directly with the rigidbody proved unreliable so we collide with an area
 # around it instead
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Pestle:
-		print_debug(body.velocity.length())
+		#print_debug(body.velocity.length())
 		if body.velocity.length() > min_split_vel:
-			
 			split()
